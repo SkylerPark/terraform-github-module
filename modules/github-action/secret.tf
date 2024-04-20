c81acbd73829cf1dba51d8912f40b5a690fc3b04
SkylerPark <012tjdals012@gmail.com>
2024-04-20 18:30:37 +0900

    fix: plaintext, encrypted try update

--- modules/github-action/secret.tf
+++ modules/github-action/secret.tf
@@ -2,6 +2,6 @@ resource "github_actions_secret" "this" {
   for_each        = var.secrets
   repository      = var.repository
   secret_name     = each.key
-  plaintext_value = each.value.plaintext_value
-  encrypted_value = each.value.encrypted_value
+  plaintext_value = try(each.value.plaintext_value, null)
+  encrypted_value = try(each.value.encrypted_value, null)
 }
